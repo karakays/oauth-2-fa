cf9c8a6db441636a90c60b69af41604c56a1bf01
Selçuk Karakayalı <skarakayali@gmail.com>
2019-04-16 16:33:08 +0300

    add curl client

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,19 +3,12 @@ CREATE TABLE OAUTH_CLIENT_DETAILS ( CLIENT_ID VARCHAR(255) PRIMARY KEY, RESOURCE
   ACCESS_TOKEN_VALIDITY INTEGER, REFRESH_TOKEN_VALIDITY INTEGER, ADDITIONAL_INFORMATION VARCHAR(4096), AUTOAPPROVE VARCHAR(255));
   
 INSERT INTO OAUTH_CLIENT_DETAILS(CLIENT_ID, RESOURCE_IDS, CLIENT_SECRET, SCOPE, AUTHORIZED_GRANT_TYPES, AUTHORITIES,
-	ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY)
-	VALUES ('webapp', 'web-resource', 'webapp', 'read,write', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000);
+	ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY) VALUES
+    ('curl', 'web-resource', 'secret', 'read,write', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000),
+	('webapp', 'web-resource', 'webapp', 'read,write', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000),
+	('iosapp', 'web-resource', 'iosapp', 'read', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000),
+	('andapp', 'admin-resource', 'andapp', 'read', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000);
  
-INSERT INTO OAUTH_CLIENT_DETAILS(CLIENT_ID, RESOURCE_IDS, CLIENT_SECRET, SCOPE, AUTHORIZED_GRANT_TYPES, AUTHORITIES,
-	ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY)
-	VALUES ('iosapp', 'web-resource', 'iosapp', 'read', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000);
-	
-INSERT INTO OAUTH_CLIENT_DETAILS(CLIENT_ID, RESOURCE_IDS, CLIENT_SECRET, SCOPE, AUTHORIZED_GRANT_TYPES, AUTHORITIES,
-	ACCESS_TOKEN_VALIDITY, REFRESH_TOKEN_VALIDITY)
-	VALUES ('andapp', 'admin-resource', 'andapp', 'read', 'password,authorization_code,refresh_token,implicit', 'USER', 10800, 2592000);
- 
-INSERT INTO user_credentials (id, password, username, secret, is_2fa_enabled)
-	VALUES (1, '$2a$10$nLrCuyqhrIK3x2M7EYIn7u7LEaXbAJTvPuY4QQeBBvdnuPn4j9kqe', 'alice', NULL, false);
-
-INSERT INTO user_credentials (id, password, username, secret, is_2fa_enabled) 
-	VALUES (2, '$2a$10$nLrCuyqhrIK3x2M7EYIn7u7LEaXbAJTvPuY4QQeBBvdnuPn4j9kqe', 'bob', 'JBSWY3DPEHPK3PXP', true);
+INSERT INTO user_credentials (id, password, username, secret, is_2fa_enabled) VALUES
+	(1, '$2a$10$nLrCuyqhrIK3x2M7EYIn7u7LEaXbAJTvPuY4QQeBBvdnuPn4j9kqe', 'alice', NULL, false),
+	(2, '$2a$10$nLrCuyqhrIK3x2M7EYIn7u7LEaXbAJTvPuY4QQeBBvdnuPn4j9kqe', 'bob', 'JBSWY3DPEHPK3PXP', true);
